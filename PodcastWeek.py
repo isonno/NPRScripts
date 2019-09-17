85d190e232c0a5db5e669bc680a040d162f7dec5
John Peterson <jp@adobe.com>
2019-09-16 23:16:54 -0700

    Convert from Python 2 to Python 3
    Should really switch to using Requests instead of the mess 2to3 created with `urllib`, but whatever.

--- PodcastWeek.py
+++ PodcastWeek.py
@@ -20,7 +20,7 @@
 # John Peterson, Nov 2017. This is a major re-write of "GetMktplc.py"
 #
 
-import string, time, urllib, urllib2, os, sys
+import string, time, urllib.request, urllib.parse, urllib.error, urllib.request, urllib.error, urllib.parse, os, sys
 import xml.dom.minidom as xml
 import datetime, string, glob
 
@@ -36,9 +36,9 @@ def getData(item, key, attr=None):
     itemData = itemData[0]
 
     if (attr):
-        return itemData.getAttribute(attr).encode('utf-8')
+        return itemData.getAttribute(attr)
     else:
-        return itemData.firstChild.data.encode('utf-8') if itemData.firstChild else None
+        return itemData.firstChild.data if itemData.firstChild else None
 
 # One podcast show
 class Show:
@@ -71,30 +71,30 @@ class Show:
         dstpath = DestDrive + os.sep + folder + os.sep + dstName
         dupe_count = 0
         while (os.path.exists(dstpath) and dupe_count < 20):
-            print "# Already have %s" % dstName
+            print("# Already have %s" % dstName)
             dstpath = DestDrive + os.sep + folder + os.sep + ("%02d_" % dupe_count) + dstName
             dupe_count += 1
         if self.data['enclosure_url']:
             if (dupe_count > 0):
-                print "# Writing show %s\n  (%s)" % (("%02d_" % dupe_count) + dstName, self.data['title'])
+                print("# Writing show %s\n  (%s)" % (("%02d_" % dupe_count) + dstName, self.data['title']))
             else:
-                print "# Writing show %s\n  (%s)" % (dstName, self.data['title'])
+                print("# Writing show %s\n  (%s)" % (dstName, self.data['title']))
             showStream = None
             try:
-                showStream = urllib2.urlopen(self.data['enclosure_url'])
+                showStream = urllib.request.urlopen(self.data['enclosure_url'])
                 showMP3 = showStream.read()
-                file(dstpath, 'wb').write(showMP3)
-            except urllib2.URLError as err:
-                print "### RE-DIRECT Failure?? %s" % str(err)
-                print "Original URL %s" % self.data['enclosure_url']
+                open(dstpath, 'wb').write(showMP3)
+            except urllib.error.URLError as err:
+                print("### RE-DIRECT Failure?? %s" % str(err))
+                print("Original URL %s" % self.data['enclosure_url'])
                 if (showStream):
-                    print "Redirect URL %s" % showStream.geturl()
+                    print("Redirect URL %s" % showStream.geturl())
         else:
-            print "# No MP3 available for (%s)" % self.data['title']
+            print("# No MP3 available for (%s)" % self.data['title'])
 
     # Debug
     def ShowKeys(self):
-        print string.join([k for k in self.data.keys() if (self.data[k])], " : ")
+        print(string.join([k for k in list(self.data.keys()) if (self.data[k])], " : "))
 
 # Podcast feed info
 class PodFeed:
@@ -104,16 +104,16 @@ class PodFeed:
         self.baseName = baseName
         xmltext = ""
         try:
-            xmltext = urllib.urlopen( feedURL ).read()
+            xmltext = urllib.request.urlopen( feedURL ).read()
         except IOError:
-            print "Failed to read: %s" % feedURL
+            print("Failed to read: %s" % feedURL)
             sys.exit(0)
 
         xmldata = xml.parseString( xmltext )
         for k in ["title", "link", "description"]:
             self.__dict__[k] = getData(xmldata, k)
 
-        print "Loading RSS for %s" % self.title
+        print("Loading RSS for %s" % self.title)
 
         # For now, assume shows are in reverse chron order,
         # we could sort to be super sure...
@@ -124,7 +124,7 @@ def getWindowsDrives():		# http://stackoverflow.com/questions/827371
 	from ctypes import windll
 	drives = []
 	bitmask = windll.kernel32.GetLogicalDrives()
-	for letter in string.uppercase:
+	for letter in string.ascii_uppercase:
 		if bitmask & 1:
 			drives.append(letter + ":")
 		bitmask >>= 1
@@ -138,7 +138,7 @@ def setDestinationDrive():
 	if sys.platform == "darwin":
 		DestDrive = "/Volumes/AUDIO"
 		if not os.path.exists(DestDrive):
-			print "Thumb drive %s not plugged in?" % DestDrive
+			print("Thumb drive %s not plugged in?" % DestDrive)
 			sys.exit(-1)
 	else:
 		winDrives = getWindowsDrives()[1:]	# Skip the C: drive
@@ -147,7 +147,7 @@ def setDestinationDrive():
 				DestDrive = d
 				break
 		if (not DestDrive):
-			print "NPR Thumb drive missing?  It must have a MKTPLC folder"
+			print("NPR Thumb drive missing?  It must have a MKTPLC folder")
 			sys.exit(-1)
 
 # Return the date of the last specified weekday (0=Mon, 1=Tue...6=Sun)
@@ -160,17 +160,17 @@ def clean():
 	for fold in folders:
 		path = DestDrive + os.path.sep + fold + os.path.sep
 		if not os.path.exists( path ):
-			print "Creating folder %s..." % path
+			print("Creating folder %s..." % path)
 			os.mkdir( path )
 		else:
-			print "Cleaning %s..." % path
+			print("Cleaning %s..." % path)
 			[os.remove(path + f) for f in os.listdir( path )]
 
 def downloadShows():
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
-    feedtext = file("RSSFeeds.txt",'r').readlines()
+    feedtext = open("RSSFeeds.txt",'r').readlines()
     feeds = []
-    print "# Loading RSS feeds..."
+    print("# Loading RSS feeds...")
     for t in feedtext:
         if (t[0] != "#"):
             args = (t[:-1]).split(",")
